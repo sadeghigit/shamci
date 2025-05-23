372136ddbb21c323c0644e3dea0c241ebed7efa8
Hamed Sadeghi <sadeghigit@gmail.com>
2025-05-23 16:27:06 +0330

    install vazirmatn font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,4 +6,11 @@ export default {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-sans)'],
+      },
+    },
+  },
 } satisfies Config;
